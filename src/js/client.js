6c96320019c1980fe9ea9da8b8ef83b164b9b512
Jack Tan <Jack Tan>
2016-08-06 15:19:59 +1200

    Moved index.html into a folder called 'html'

--- src/js/client.js
+++ src/js/client.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 class Layout extends React.Component {
     render() {
         return (
-            <h1>PHUCTHEPHUCKINGWEB, WHY IS THIS STYLED</h1>
+            <h1>KAMI SAMA = JANIDHU</h1>
         );
     }
 }
