af882d471512fd73d2ed915c02a1566bb58ecf1a
Jack Tan <Jack Tan>
2016-08-11 22:33:11 +1200

    Added a text shadow on the links on the header menu; changed the button design a little bit (still ugly, will need to update it, WIP); added a points text; added a slider which is the place where the images will move to the left; when the 'How to play link' is clicked, it redisplayed the instruction panel; clicking on the submit button will fade out the first image (repeated clicks results in repeated fade outs of the next image); added a fail panel (havent added the logic to display it when you fail the game, just added it there to be used later)

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,16 @@
 		element.style.display = display;
 	}
 
-  	var startCountdown = function(element, value) {
+  	var startCountdown = function(element, value, callback) {
+  		var body = document.getElementsByTagName('body')[0];
+  		//body.style.webkitFilter = 'grayscale(100%) blur(3px)';
+  		//element.style.webkitFilter = 'grayscale(0%) blur(0px)';
     	var countDownTimer = setInterval(function() {
       		if (value === 0) {
         		clearInterval(countDownTimer);
         		modifyElementDisplay(element, 'none');
+        		element.innerHTML = "";
+        		callback();
       		}
       		element.innerHTML = value--;
     	}, 1000);
@@ -25,8 +30,8 @@
     	}
   	}
 
-	var togglePanel =  function(element) {
-	    var display = window.getComputedStyle(element,null).getPropertyValue("display");
+	var togglePanel = function(element) {
+	    var display = element.style.display;
 	    if (isVisible(display)) {
 	      	modifyElementDisplay(element, 'none');
 	    } else {
@@ -48,23 +53,65 @@
 			return false;
   		}
   	}
-  
+
+  	var slider = function(element) {
+  		console.log(element.style);
+  	    element.style.transform = 'translate(-100%)';
+	    element.style.transition = 'transform 5s linear';
+	    //element.style.marginRight = '100%';
+	    //element.style.transition = '3s';
+  	}
+
+  	var destroy = function(image) {
+  		image.classList.add('destroy');
+  	}
+
   	// =======================================================
 
     var countdown_panel = document.getElementById('countdown_panel');
+	var image = document.getElementsByClassName('image');
+  	var submit_textfield = document.getElementById('submit_user_input');
+  	var instruction_panel = document.getElementsByClassName('instruction')[0];
+	var images = document.getElementsByClassName('images')[0];
+	var points = document.getElementsByClassName('points')[0];
 
   	var start_button = document.getElementById('start_button');
   	start_button.onclick = function() {
-    	togglePanel(start_button.parentElement);
+    	togglePanel(instruction_panel);
     	modifyElementDisplay(countdown_panel, 'flex');
-  		startCountdown(countdown_panel, 3);
+  		startCountdown(countdown_panel, 3, function() {
+  			slider(images);
+  			//destroy(points);
+  		});
   	}
 
-  	var submit_textfield = document.getElementById('submit_user_input');
+	//slider(images);
+
+	var imageIteration = 0;
   	var submit_button = document.getElementById('submit_button');
   	submit_button.onclick = function() {
-  		checkIfUserInputIsValid(submit_textfield);
+  		if (!checkIfUserInputIsValid(submit_textfield)) {
+  			destroy(image[imageIteration]);
+  			imageIteration++;
+  		}
   	}
 
+  	var how_to_play_link = document.getElementById('how_to_play_link');
+  	how_to_play_link.onclick = function() {
+		togglePanel(instruction_panel);
+  	};
 
+  	var hide = function() {
+	  	countdown_panel.style.display = 'none';
+	  	var instruction = document.getElementsByClassName('instruction')[0];
+	  	instruction.style.display = 'none';		
+  	}
+
+  	var hideFailPanel = function() {
+  		var failPanel = document.getElementsByClassName('failPanel')[0];
+  		failPanel.style.display = 'none';
+  	}
+
+  	hide();
+  	hideFailPanel();
 })();
