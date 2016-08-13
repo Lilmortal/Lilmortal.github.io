ec78a90ae53a66c5d34a2e3f57456c2a285a88d1
Jack Tan <Jack Tan>
2016-08-13 21:44:21 +1200

    Added animation when fail panel is displayed; changed the instruction panel layout; added a scroll to the instruction panel if screen size is too big; when it is counting down, the background slowly turns gray until the countdown finishes

--- src/js/index.js
+++ src/js/index.js
@@ -21,54 +21,41 @@
 
 	var IS_NUMERIC = new RegExp(/^\d+$/);
 
-	var modifyElementDisplay = function(element, display) {
-		element.style.display = display;
+	var enableElement = function(element) {
+		element.style.display = 'flex';
+	}
+
+	var disableElement = function(element) {
+		element.style.display = 'none';
+	}
+
+	var toggleClass = function(element, className) {
+		if (element.classList.contains(className)) {
+  			element.classList.remove(className);
+  			// weird hack rule - https://css-tricks.com/restart-css-animation/
+  			void element.offsetWidth;
+  		}
+  		element.classList.add(className);
 	}
 
   	var startCountdown = function(element, value, callback) {
   		element.innerHTML = "";
-  		var body = document.getElementsByTagName('body')[0];
-  		//body.style.webkitFilter = 'grayscale(100%) blur(3px)';
-  		//element.style.webkitFilter = 'grayscale(0%) blur(0px)';
     	var countDownTimer = setInterval(function() {
       		if (value === 0) {
         		clearInterval(countDownTimer);
-        		
-        		modifyElementDisplay(element, 'none');
+        		disableElement(element);
         		callback();
       		}
       		element.innerHTML = value--;
     	}, 1000);
   	}
 
-  	var isVisible = function(display) {  
-    	if (display !== 'none' && display !== 'undefined') {
-      		return true;
-    	} else {
-      		return false;
-    	}
-  	}
-
-	var togglePanel = function(element) {
-	    var display = element.style.display;
-	    if (isVisible(display)) {
-	      	modifyElementDisplay(element, 'none');
-	    } else {
-	      	modifyElementDisplay(element, 'flex');
-	    }
-	}
-
   	var checkIfUserInputIsValid = function(element) {
-  		if (element.classList.contains('shake_textfield')) {
-  			element.classList.remove('shake_textfield');
-  			// weird hack rule - https://css-tricks.com/restart-css-animation/
-  			void element.offsetWidth;
-  		}
 		var value = element.value;
 		if (IS_NUMERIC.test(value)) {
 			return true;
 		} else {
-			element.classList.add('shake_textfield');
+			toggleClass(element, 'shake_textfield');
 			return false;
   		}
   	}
@@ -101,21 +88,6 @@
 	    element.addEventListener(transitionEvent, callback);
   	}
 
-  	var destroy = function(image) {
-  		image.style.display = 'none';
-  		//image.classList.add('destroy');
-  	}
-
-  	var displayFailPanel = function(panel) {
-  		panel.style.display = 'flex';
-  		var failText = document.getElementsByClassName('failText')[0];
-  		var tryAgainButton = document.getElementById('tryAgainButton');
-  		failText.classList.add('display_fail_text');
-  		tryAgainButton.style.display = 'block';
-  		/*tryAgainButton.style.transform = 'scale(2,2)';
-  		tryAgainButton.style.transition = 'transform 2s ease-in-out';*/
-  	}
-
   	// =======================================================
 
     var countdown_panel = document.getElementById('countdown_panel');
@@ -124,29 +96,33 @@
   	var instruction_panel = document.getElementsByClassName('instruction')[0];
 	var images = document.getElementsByClassName('images')[0];
 	var points = document.getElementsByClassName('points')[0];
-	var failPanel = document.getElementsByClassName('failPanel')[0];
+	var failPanel = document.getElementsByClassName('failBackground')[0];
+	
+	var wrapper = document.getElementsByClassName('wrapper')[0];
 
   	var start_button = document.getElementById('start_button');
   	start_button.onclick = function() {
-    	togglePanel(instruction_panel);
-    	modifyElementDisplay(countdown_panel, 'flex');
+  		toggleClass(wrapper, 'grayscale_background');
+    	disableElement(instruction_panel);
+    	enableElement(countdown_panel);
   		startCountdown(countdown_panel, 3, function() {
   			slider(images);
   			transitionEnd(images, function() {
-  				displayFailPanel(failPanel);
+  				enableElement(failPanel);
   			})
   		});
   	}
 
   	var tryAgainButton = document.getElementById('tryAgainButton');
   	tryAgainButton.onclick = function() {
-  		modifyElementDisplay(failPanel, 'none');
-  		modifyElementDisplay(countdown_panel, 'flex');
+  		toggleClass(wrapper, 'grayscale_background');
+  		disableElement(failPanel);
+  		enableElement(countdown_panel);
   		restartSlider(images, image);
   		startCountdown(countdown_panel, 3, function() {
 			slider(images);
   			transitionEnd(images, function() {
-  				displayFailPanel(failPanel);
+  				enableElement(failPanel);
   			});
   		});
   	}
@@ -155,14 +131,14 @@
   	var submit_button = document.getElementById('submit_button');
   	submit_button.onclick = function() {
   		if (!checkIfUserInputIsValid(submit_textfield)) {
-  			destroy(image[imageIteration]);
+  			disableElement(image[imageIteration]);
   			imageIteration++;
   		}
   	}
 
   	var how_to_play_link = document.getElementById('how_to_play_link');
   	how_to_play_link.onclick = function() {
-		togglePanel(instruction_panel);
+		enableElement(instruction_panel);
   	};
 
   	var hide = function() {
@@ -172,6 +148,6 @@
   	}
 
   	//hide();
-  	modifyElementDisplay(failPanel, 'none');
-  	//displayFailPanel(failPanel);
+  	//disableElement(failPanel);
+  	//enableElement(failPanel);
 })();
